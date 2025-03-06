e23eca581d1e026853b588e3290df1a5143b7c76
Ivan <github@laodesign.es>
2025-03-06 17:24:13 +0100

    Change Screenshot filename

--- Assets/Scripts/Camera/ScreenShot.cs
+++ Assets/Scripts/Camera/ScreenShot.cs
@@ -3,5 +3,15 @@ using UnityEngine;
 
 public class ScreenShot : MonoBehaviour
 {
-    public static void TakeScreenShot() => ScreenCapture.CaptureScreenshot($"{ConfigVariables.GetConfigValue<string>(ConfigTypes.TITLE)}-{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png");
+    public static void TakeScreenShot()
+    {
+#if !UNITY_EDITOR // Don't create screenshot on debug mode
+        string title = ConfigVariables.GetConfigValue<string>(ConfigTypes.TITLE);
+        string dateFormat = DateTime.Now.ToString(ConfigVariables.GetConfigValue<string>(ConfigTypes.DATE_FORMAT));
+        string extension = ConfigVariables.GetConfigValue<string>(ConfigTypes.SCREENSHOT_EXTENSION);
+        // Replace ":" por "_"
+        string screenshot = $"{title}-{dateFormat}{extension}".Replace(":", "_");
+        ScreenCapture.CaptureScreenshot(screenshot);
+#endif
+    }
 }
