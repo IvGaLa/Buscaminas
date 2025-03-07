9792bf88e79f7a5b7d874d2ba94f313b73be9d5d
Ivan <github@laodesign.es>
2025-03-07 20:37:09 +0100

    .

--- Assets/Scripts/Camera/ScreenShot.cs
+++ Assets/Scripts/Camera/ScreenShot.cs
@@ -5,13 +5,11 @@ public class ScreenShot : MonoBehaviour
 {
     public static void TakeScreenShot()
     {
-#if !UNITY_EDITOR // Don't create screenshot on debug mode
         string title = ConfigVariables.GetConfigValue<string>(ConfigTypes.TITLE);
         string dateFormat = DateTime.Now.ToString(ConfigVariables.GetConfigValue<string>(ConfigTypes.DATE_FORMAT));
         string extension = ConfigVariables.GetConfigValue<string>(ConfigTypes.SCREENSHOT_EXTENSION);
-        // Replace ":" por "_"
+        // Replace ":" for "_"
         string screenshot = $"{title}-{dateFormat}{extension}".Replace(":", "_");
         ScreenCapture.CaptureScreenshot(screenshot);
-#endif
     }
 }
